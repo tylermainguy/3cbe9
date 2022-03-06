cb7712ce85a83d92bd27d132e49128954a38de0e
Tyler Mainguy <tsmainguy@outlook.com>
2022-03-06 17:55:03 -0500

    db-redesign - #comment fix bug in import, add migration file

--- server/messenger_backend/models/__init__.py
+++ server/messenger_backend/models/__init__.py
@@ -1,5 +1,5 @@
 from .conversation import Conversation
-from .conversationMembers import ConversationMember
+from .conversationMember import ConversationMember
 from .message import Message
 from .messageRead import MessageRead
 from .user import User
