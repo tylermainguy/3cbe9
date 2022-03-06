f3563c8b7edaeaa01cf53be462e231c92c189dfd
Tyler Mainguy <tsmainguy@outlook.com>
2022-03-06 17:13:43 -0500

    db-redesign - #comment add database models

--- server/messenger_backend/models/__init__.py
+++ server/messenger_backend/models/__init__.py
@@ -1,4 +1,5 @@
 from .conversation import Conversation
+from .conversationMembers import ConversationMember
 from .message import Message
 from .messageRead import MessageRead
 from .user import User
